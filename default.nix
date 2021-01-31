ca84120dcbb194c265bad36f61e2483e024eec99
Robert T. McGibbon <rmcgibbo@gmail.com>
2021-01-31 18:08:40 -0500

    Forgot gzip?

--- default.nix
+++ default.nix
@@ -29,7 +29,7 @@ python3.pkgs.buildPythonApplication rec {
     mypy --strict nixpkgs_review
   '';
   makeWrapperArgs = [
-    "--prefix PATH : ${stdenv.lib.makeBinPath [ nixFlakes git curl gnutar ]}"
+    "--prefix PATH : ${stdenv.lib.makeBinPath [ nixFlakes git curl gnutar gzip ]}"
     "--set NIX_SSL_CERT_FILE ${cacert}/etc/ssl/certs/ca-bundle.crt"
     # we don't have any runtime deps but nix-review shells might inject unwanted dependencies
     "--unset PYTHONPATH"
